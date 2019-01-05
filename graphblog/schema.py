6540ef36404aa6a7674ea82d1a0ebddabb7e1371
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-05 12:32:36 -0800

    Initial Commit.

---
+++ graphblog/schema.py
@@ -0,0 +1,18 @@
+"""Schema for the main application."""
+
+
+import graphene
+import blog.schema
+
+
+QUERIES = (
+    # Place all future query classes here.
+    blog.schema.Query,
+)
+
+class Query(*QUERIES):
+    """Top level query class that inherits from all others."""
+    pass
+
+
+schema = graphene.Schema(query=Query)
